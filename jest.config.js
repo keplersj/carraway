6e8d26838e69e0a58644c0cf7c211bf18e9ea0e6
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-11-04 17:17:15 -0700

    test: add jest+prettier

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    {
+      displayName: "lint:prettier",
+      preset: "jest-runner-prettier",
+      testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+    }
+  ]
+};
